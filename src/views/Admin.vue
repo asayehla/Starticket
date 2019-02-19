25d7a3ca64a147a263feac96ce42f4a506ff1ca5
reeklof <reeklof@live.com>
2019-02-19 16:04:40 +0100

    rebuild admin and removed admin from components

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,135 +1,144 @@
 <template lang="html">
-    <main>
-    <section>
+    <main id="admin">
         <h1>Admin Page</h1>
-    </section>
-    <section>
-        <div class="table">
-            <p>Name</p>
-            <p>Where</p>
-            <p>Available Seats</p>
-            <p>Sold Tickets</p>
-            </div>
-            <admin-item v-for="event in events" :key="event.id" :event="event" />
-    </section>
-
-
-    <!--Form to add new events-->
-    <section>
-        <form class="add-event">
-        <h3>Add Event</h3>
-            <label>
-                <input type="text" v-model="add.name" placeholder="Name">
-            </label>
-            <label>
-                <input type="text" v-model="add.where" placeholder="Where">
-            </label>
-            <label>
-                <input type="text" v-model="add.date" placeholder="Date">
-            </label>
-            <label>
-                <input type="number" v-model="add.from" placeholder="From Time">
-            </label>
-            <label>
-                <input type="number" v-model="add.to" placeholder="To Time">
-            </label>
-            <label>
-                <input type="text" v-model="add.seats" placeholder="# Seats">
-            </label>
-            <br>
-            <button @click="submit">Add The Event</button>
-        </form> 
-    </section>
-
-    </main>
+        <section class="container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Where</th>
+                        <th>Availabel Seats</th>
+                        <th>Sold Tickets</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="event in events" :key="event._id">
+                        <td>{{event.name}}</td>
+                        <td>{{event.where.venue}}</td>
+                        <td>{{event.tickets.available}}</td>
+                        <td>{{event.tickets.sold}}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <aside class="form">
+                <input type="text" placeholder="event name" v-model="newEvent.name">
+                <input type="text" placeholder="event location" v-model="newEvent.where.venue">
+                <input type="text" placeholder="start time" v-model="newEvent.when.from">
+                <input type="text" placeholder="end time" v-model="newEvent.when.to">
+                <input type="text" placeholder="price" v-model="newEvent.price">
+                <input type="number" placeholder="total tickets" v-model="newEvent.tickets.available">
+                <a href="#" class="btn" @click="createEvent">Create event</a>
+            </aside>
+        </section>
+    </main> 
 </template>
 
-<script>
-import adminItem from '@/components/admin-item';
 
+<script>
 export default {
-  data() {
-    return {
-      add: {
-        name: '',
-        where: '',
-        date: '',
-        from: '',
-        to: '',
-        seats: '',
-      }
-    }
-  },
     name:'admin',
-  components: {
-        adminItem
-  },
-  computed: {
+    data () {
+        return {
+            newEvent: {
+                name: '',
+                where: {
+                    venue: ''
+                },
+                when: {
+                    date: '',
+                    from: '',
+                    to: '',
+                },
+                price: 0,
+                tickets: {
+                    available: 0,
+                    sold: 0
+                }
+            }
+        }
+    },
+    methods: {
+    async createEvent() {
+        try {
+    
+            await this.$http.post('http://localhost:3000/events', this.newEvent);
+            this.$store.dispatch('getEvents');
+        
+        } catch(err) {
+            alert('Något sket sig!');
+        }
+
+        }
+    },
+      computed: {
     events() {
       return this.$store.state.events;
-    }
-  },
-
-  methods: {
-    submit() {
-      // Send data to the server, or update your store.
     }
   }
 }
 </script>
 
 <style lang="scss">
-.table {
-    display: flex; 
-    justify-content: center; 
-    margin: 5px 30px;
-    background-color: rgb(0,0,65);
-    p {
-    font-size: 1rem;
-    color: #F56B9A;
-    width: 200px;
-    margin: 20px 0;
-    border-bottom: grey .1rem solid; 
-    text-align: left;
-  }
-  li {
-      color: red;
-  }
-}
-//Form - "Add Event"
-h3 {
-    color: #F56B9A;
-    font-size: 1.7rem;
+#admin {
+    background:rgb(0,0,65);
     display: flex;
-    margin: 8px auto;
-    box-sizing: border-box;
     justify-content: center;
-}
-.add-event {
-    background-color: rgb(0,0,65);
-    padding: 15px 0;
-    margin: 8px 30px;
-    input  {
-        min-width: 85%;
-        border-radius: 3px;
-        font-size: .7rem;
-        border: 0.5px solid grey;
-        background-color: rgb(0,0,80);
-        color: white; 
-        padding: 10px 15px;
-        margin: 8px 0;
+
+    .logo {
+        position: fixed;
+        margin: 1rem; 
+        width: 2rem;
+        left: 0;
     }
-}
-button {
-    background-color: #F56B9A;
-    font-size: 1rem;
-    font-weight: bold;
-    cursor: pointer;
-    min-width: 80%; 
-    color: white; 
-    padding: 8px 10px; 
-    margin: 8px 0; 
-    border-radius: 4px;
-    border: none; 
+
+    .container {
+        display: flex; 
+        grid-template-columns: 4fr 1fr; 
+        max-width: 1000px;
+        width: 100%; 
+        color: white;
+
+        table {
+            background: darkblue; 
+            padding: 1rem;
+
+            thead {
+                tr {
+                    th {
+                        color: pink; 
+                        text-align: left;
+                        border-bottom: 1px solid green; 
+                        padding: .5rem 0;
+                    }
+                }
+            }
+            thead {
+                tr {
+                    td {
+                        color: white; 
+                        padding: 1rem 0;
+                    }
+                    &:nth-child(1) {
+                        background: white;
+                    }
+                }
+            }
+        }
+        .form {
+            background: rgb(0,0,65);
+            padding: 1rem;
+
+            input {
+                width: 100%;
+                background: none; 
+                border: 1px solid red; 
+                border-radius: 3px;
+                padding: .25rem;
+                font-size: 1.2rem;
+                color: white;
+                margin: 0 0 1rem 0;
+            }
+        }
+    }
 }
 </style>
