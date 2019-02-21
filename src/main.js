82b71f179d9a843ebea4eed9e77643b358fccd47
Åsa Palholmen <asa@palholmen.se>
2019-02-21 14:42:53 +0100

    added font awesome

--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
- 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Importing FontAwesome icons, here is a library.add needed for each icon you want included
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon); // skapar syntax för fontawesome. (?)
+
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
