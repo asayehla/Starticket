c9ac01bc318a2f146a7c7aa8543e167c2a9b0606
reeklof <reeklof@live.com>
2019-02-21 21:22:24 +0100

    made search case sensitive

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,9 +1,9 @@
 <template lang="html">
   <main id="events">
     <h1>Events</h1>
-    <input type="text" v-model="search" placeholder="Search for Event">
+    <input type="text" v-model="search" placeholder="Search for Event"/>
     <section>
-      <event-item v-for="event in filterEvent" :key="event.id" :event="event" />
+      <event-item v-for="event in filterEvent" :key="event.id" :event="event"/>
     </section>
   </main>
 </template>
@@ -31,7 +31,7 @@ export default {
     },
     filterEvent: function (){
       return this.events.filter((event) => {
-        return event.name.match(this.search)
+        return event.name.toLowerCase().match(this.search.toLowerCase())
       });
     }
   }
@@ -42,15 +42,15 @@ export default {
 @import '../scss/components';
 #events {
 
-h1 {
-  margin: 1rem;
-}
+  h1 {
+    margin: 1rem;
+  }
 
-input {
-  font-size: 1rem;
-  margin-bottom: 1rem;
-  padding: 0.5rem 1rem;
-  color: rgb(217, 217, 217);
-}
+  input {
+    font-size: 1rem;
+    margin-bottom: 1rem;
+    padding: 0.5rem 1rem;
+    color: rgb(217, 217, 217);
+  }
 }
 </style>
